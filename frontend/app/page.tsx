033dd7fdfb384f10303031614453b125e32f9d17
graviolagames <graviolagames@gmail.com>
2023-09-10 19:36:17 -0300

    Introduce bad code

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,3 +9,41 @@ return (
 )
 }
 
+function calculateSum(a, b) {
+    return a + b;
+  }
+  
+  var x = 5;
+  var y = "10";
+  
+  if (x == y) {
+    console.log("Igual");
+  } else {
+    console.log("Diferente");
+  }
+  
+  for (var i = 0; i < 10; i++) {
+    document.write(i);
+  }
+  
+  function doSomething() {
+    alert("Olá, mundo!");
+  }
+  
+  function complexLogic(a, b) {
+    if (a < b) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  var result = complexLogic(5, 10);
+  
+  
+  
+  
+  
+
+
+
