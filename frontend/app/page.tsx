4873cce9496286e431c52bb9a6be3ba3084e33b0
graviolagames <graviolagames@gmail.com>
2023-09-10 19:37:52 -0300

    Added bad code

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,21 +2,8 @@ export default function Home()
 {
         return (
                                     <div>
-
-
-
-
-
                                     <h1> Welcome to the ConchaYOro App </h1>
-
-
-
-
-
-
-
                                     <h1>Password=Senha123</h1>
-
                                     </div>
     )
 }
