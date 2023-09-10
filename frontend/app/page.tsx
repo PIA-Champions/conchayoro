e03e1597c1f6c427513a1d2584a951359636c5e2
Sobrinho <davidhiago@gmail.com>
2023-09-10 19:06:33 -0300

    Add vulnerable code

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,12 @@
-export default function Home() {
+export default function Home() 
+{
 return (
 <div>
 <h1> Welcome to the ConchaYOro App </h1>
 <h1>Password=Senha123</h1>
+
 </div>
 )
 }
+
+print abcd
