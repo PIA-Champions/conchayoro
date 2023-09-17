f663c5cb769ca4de72375961e6fa676edf72164a
David Sobrinho <43732922+DavidMSobrinho@users.noreply.github.com>
2023-09-17 16:32:04 -0300

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,46 +3,6 @@ export default function Home()
         return (
                                     <div>
                                     <h1> Welcome to the ConchaYOro App </h1>
-                                    <h1>Password=Senha123</h1>
                                     </div>
     )
 }
-
-function calculateSum(a, b) {
-    return a + b;
-  }
-  
-  var x = 5;
-  var y = "10";
-  
-  if (x == y) {
-    console.log("Igual");
-  } else {
-    console.log("Diferente");
-  }
-  
-  for (var i = 0; i < 10; i++) {
-    document.write(i);
-  }
-  
-  function doSomething() {
-    alert("Olá, mundo!");
-  }
-  
-  function complexLogic(a, b) {
-    if (a < b) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  
-  var result = complexLogic(5, 10);
-  
-  
-  
-  
-  
-
-
-
